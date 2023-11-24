f1e348649a08eee4c4a6ee46316c100b32ac8c78
thislife_won <sw8744@naver.com>
2023-11-24 20:08:08 +0900

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/main';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Main Page</h1>
+        </div>
+    );
+};
+
+export default MainPage;
